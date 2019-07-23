298cdc21668612c3a7974c00c0d84abf21417766
Nora <nora.abdelgadir@ibm.com>
2019-07-23 10:21:44 -0400

    fix eslint violations

--- test/unit.js
+++ test/unit.js
@@ -5,25 +5,25 @@
 
 'use strict';
 
-var expect = require('chai').expect;
-var apiDef = require('../lib/get-api-def');
+const expect = require('chai').expect;
+const apiDef = require('../lib/get-api-def');
 
 describe('Test function getApiDef', function() {
-  var sampleAppJs = require('./fixtures/app.js');
+  const sampleAppJs = require('./fixtures/app.js');
 
   it('No format specified', function() {
-    var value = apiDef.getApiDef(sampleAppJs, {});
+    const value = apiDef.getApiDef(sampleAppJs, {});
     expect(value).to.contain('swagger: \'2.0\'');
   });
 
   it('Yaml format', function() {
-    var value = apiDef.getApiDef(sampleAppJs, {format: 'yaml'});
+    const value = apiDef.getApiDef(sampleAppJs, {format: 'yaml'});
     expect(value).to.contain('swagger: \'2.0\'');
     expect(value).to.not.contain('!<tag:yaml.org,2002:js/undefined> \'\'');
   });
 
   it('JSON format', function() {
-    var value = apiDef.getApiDef(sampleAppJs, {format: 'JSON'});
+    const value = apiDef.getApiDef(sampleAppJs, {format: 'JSON'});
     expect(value).to.contain('"swagger":"2.0"');
   });
 });
